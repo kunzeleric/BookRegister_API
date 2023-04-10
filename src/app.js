9f096bb869f435d7a0e68438941ecc751d132359
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-04-10 16:47:21 -0300

    Adicionando métodos de busca, inclusão e de alteração de livro

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
-    {id: 1, titulo: "Senhor dos Aneis"},
-    {id: 2, titulo: "O Hobbit"}
+    {id: 1, "titulo": "Senhor dos Aneis"},
+    {id: 2, "titulo": "O Hobbit"}
 ]
 
 app.get('/', (req, res) => {
@@ -15,5 +17,25 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 })
+
+app.get('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.json(livros[index]); //retorna o json com o livro especificado na busca 
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso!');
+})
+
+app.put('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros); //resposta é o array 'livros' em formato json
+})
+
+const buscaLivro = (id) => {
+    return livros.findIndex(livro => livro.id == id);
+}
 
 export default app;
