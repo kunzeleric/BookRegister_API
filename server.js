7d53a147e566cbf154a6a3f7b393c1d49c0e971b
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-04-10 16:01:30 -0300

    Instalando e utilizando o expressJS

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
+import app from './src/app.js'; //utilizando o express
+
 //criando um servidor local utilizando somente NodeJS
 
-const http = require('http'); //importando 
-const port = 3000;
+//const http = require('http'); //importando 
+const port = process.env.PORT || 3000;
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na página de livros',
-    '/autores': 'Listagem de Autores',
-    '/editora': 'Página da Editora'
-}
+app.listen(3000, () => console.log(`Servidor rodando na porta ${port}`));
 
+// const rotas = {
+//     '/': 'Curso de Node',
+//     '/livros': 'Entrei na página de livros',
+//     '/autores': 'Listagem de Autores',
+//     '/editora': 'Página da Editora'
+// }
+// 
+// 
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/plain'}); //define no cabeçalho o tipo de dado a ser passado
+//     res.end(rotas[req.url]); //dependendo da url será chamada uma rota específica
+// })
+//server.listen(3000, () => console.log(`Servidor rodando na porta ${port}`));
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'}); //define no cabeçalho o tipo de dado a ser passado
-    res.end(rotas[req.url]); //dependendo da url será chamada uma rota específica
-})
-
-server.listen(3000, () => console.log(`Servidor rodando na porta ${port}`));
