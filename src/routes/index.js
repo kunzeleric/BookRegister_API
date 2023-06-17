1c21c32fe599285fd9aa0697cbcfd5810931ad4a
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-06-17 12:18:53 -0300

    Finalizadas rotas de atualizar e deletar um Livro.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,6 @@ import livros from './livrosRoutes.js';
 import autores from './autoresRoutes.js';
 
 const routes = (app) => {
-    app.route('/').get( async (req, res) => {
-        res.status(200).send({titulo: 'Curso de Node'});
-    })
-
     app.use(
         express.json(),
         livros,
